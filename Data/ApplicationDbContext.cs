d804bca209aeafffc458b76d49609a50f471826c
Andre Alencar <aaugusto310@gmail.com>
2020-04-08 14:23:14 -0300

    Configuração com MySql Banco na Nuvem

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AlimentosMarfim.Models.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlimentosMarfim.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public DbSet<Setor> Setores { get; set; }
+        public DbSet<Turno> Turnos { get; set; }
+        public DbSet<Cargo> Cargos { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
